da5bce1cc56f2525cda3db331b1880df4dea979a
DAHAI LI <blizzzrdda@gmail.com>
2022-04-30 14:43:54 -0400

    + Added a interactable interface

---
+++ Assets/Scripts/Interactables/IInteractable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Interactables
+{
+    /**
+     * This class is useful for the hamster to access the tube, jump pad and nuts.
+     */
+    public interface IInteractable
+    {
+        public bool IsInteractable(GameObject source);
+
+        public bool TryInteract(GameObject source);
+
+        public void OnInteractionFinished(GameObject source);
+    }
+}
