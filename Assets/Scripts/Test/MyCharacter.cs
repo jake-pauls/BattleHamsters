66bbb0c2ef8773b015fa4b65ca11afabbd3718fd
Kab Thangarajah <kabilanm.thangarajah@gmail.com>
2022-05-05 00:15:51 -0700

    test with rigid bodies

--- Assets/Scripts/Test/MyCharacter.cs
+++ Assets/Scripts/Test/MyCharacter.cs
@@ -22,17 +22,18 @@ public class MyCharacter : MonoBehaviour
     public GameObject bulletPrefab;
 
     private Player player; // The Rewired Player
-    private CharacterController cc;
+    //private CharacterController cc;
     private Vector3 moveVector;
     private bool interact;
+    private Rigidbody _rb;
 
     void Awake()
     {
         // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
         player = ReInput.players.GetPlayer(playerId);
-
+        _rb = GetComponent<Rigidbody>();
         // Get the character controller
-        cc = GetComponent<CharacterController>();
+        //cc = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -47,16 +48,17 @@ public class MyCharacter : MonoBehaviour
         // whether the input is coming from a joystick, the keyboard, mouse, or a custom controller.
 
         moveVector.x = player.GetAxis("Move Horizontal"); // get input by name or action id
-        moveVector.y = player.GetAxis("Move Vertical");
+        moveVector.z = player.GetAxis("Move Vertical");
         interact = player.GetButtonDown("Interact");
     }
 
     private void ProcessInput()
     {
         // Process movement
-        if (moveVector.x != 0.0f || moveVector.y != 0.0f)
+        if (moveVector.x != 0.0f || moveVector.z != 0.0f)
         {
-            cc.Move(moveVector * moveSpeed * Time.deltaTime);
+            //cc.Move(moveVector * moveSpeed * Time.deltaTime);
+            _rb.AddForce(moveVector * moveSpeed);
         }
 
         // Process fire
